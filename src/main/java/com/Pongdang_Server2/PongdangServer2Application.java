13c262c17eafa8e6fd666dfb4942af307ff8387e
momo990305@gmail.com <momo990305@gmail.com>
2023-04-14 14:54:39 +0900

    First Commit

---
+++ src/main/java/com/Pongdang_Server2/PongdangServer2Application.java
@@ -0,0 +1,13 @@
+package com.Pongdang_Server2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongdangServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongdangServer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pongdang_Server2/PongdangServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pongdang_Server2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PongdangServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
