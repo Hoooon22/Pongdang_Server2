313d5b83dda2dfe59930501c25a68899d4ccf0e6
momo990305@gmail.com <momo990305@gmail.com>
2023-04-15 22:46:11 +0900

    React day01

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { BrowserView, MobileView } from 'react-device-detect'
 
 function App() {
    const [hello, setHello] = useState('')
@@ -12,7 +13,13 @@ function App() {
 
     return (
         <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
+            <BrowserView>
+                데스크톱 화면 입니다
+            </BrowserView>
+            <MobileView>
+                모바일 화면 입니다.
+            </MobileView>
+            백엔드에서 가져온 데이터입니다?! : {hello}
         </div>
     );
 }
