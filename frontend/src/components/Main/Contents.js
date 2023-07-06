a1b003f42104b0ab306dc30d2b4b0e274b2bbc67
momo990305@gmail.com <momo990305@gmail.com>
2023-07-06 21:41:53 +0900

    add title_img

--- frontend/src/components/Main/Contents.js
+++ frontend/src/components/Main/Contents.js
@@ -4,7 +4,7 @@ import "./Contents.css";
 export default function Contents() {
     return (
         <div className="contents">
-            <img className="title_img" src="/imgs/Main/MainTop/1.png"/>
+            <img className="title_img" src="/imgs/Main/1_1.png"/>
         </div>
     );
 }
