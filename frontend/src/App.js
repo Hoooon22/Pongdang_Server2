041ad09c559e175756af34ed23186f6a81f23c0d
momo990305@gmail.com <momo990305@gmail.com>
2023-07-17 14:24:06 +0900

    font upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { BrowserView, MobileView } from 'react-device-detect'
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { MainTop, Contents } from "./components/Main";
 
--- frontend/src/components/Main/Contents.js
+++ frontend/src/components/Main/Contents.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Contents.css";
+import Button from 'react-bootstrap/Button';
 
 export default function Contents() {
     return (
         <div className="contents">
             <img className="title_img" src="/imgs/Main/1_1.png"/>
             <div className="textbox">
-            <h1>두려움</h>
+            <h1>두려움</h1>
             <p>에디터가 이 글을 추천하는 이유는 추천하기 때문인데요 아무리 생각
                해와노 주선하는 이유는 주선하기
                때문입니다 실제 예를 들어서 지금
