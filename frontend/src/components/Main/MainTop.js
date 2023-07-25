5f25cfcc0125c246ebd9f993c988c036feab9a63
momo990305@gmail.com <momo990305@gmail.com>
2023-07-25 22:52:29 +0900

    MainImage_2 logo image & hr

--- frontend/src/components/Main/MainTop.js
+++ frontend/src/components/Main/MainTop.js
@@ -4,8 +4,8 @@ import "./MainTop.css";
 export default function MainTop() {
     return (
         <div className="topDiv">
-            <img className="logo" src="/imgs/Main/MainImage_2/1_퐁당로고.png"/>
-            <hr/>
+            <img className="logo" src="/imgs/Main/MainImage2/1_퐁당로고.png"/>
+            <img className="hr_image" src="/imgs/Main/MainImage2/2_구분선.png"/>
             <div className="menu">
                 <ul>
                     <li>주제글</li>
