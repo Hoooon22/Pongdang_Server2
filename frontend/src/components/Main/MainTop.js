cb13a8c98f1e26eebd1440c85a271ff29942a23e
momo990305@gmail.com <momo990305@gmail.com>
2023-07-25 23:26:42 +0900

    MainImage_2 menu image

--- frontend/src/components/Main/MainTop.js
+++ frontend/src/components/Main/MainTop.js
@@ -4,15 +4,13 @@ import "./MainTop.css";
 export default function MainTop() {
     return (
         <div className="topDiv">
-            <img className="logo" src="/imgs/Main/MainImage2/1_퐁당로고.png"/>
-            <img className="hr_image" src="/imgs/Main/MainImage2/2_구분선.png"/>
+            <img className="logo" src="/imgs/Main/MainImage2/1_logo.png"/>
+            <img className="hr_image" src="/imgs/Main/MainImage2/2_hrLine.png"/>
             <div className="menu">
-                <ul>
-                    <li>주제글</li>
-                    <li>인터뷰</li>
-                    <li>기획</li>
-                    <li>유튜브</li>
-                </ul>
+                <img src="/imgs/Main/MainImage2/3_menu1.png"/>
+                <img src="/imgs/Main/MainImage2/3_menu2.png"/>
+                <img src="/imgs/Main/MainImage2/3_menu3.png"/>
+                <img src="/imgs/Main/MainImage2/3_menu4.png"/>
             </div>
         </div>
     );
