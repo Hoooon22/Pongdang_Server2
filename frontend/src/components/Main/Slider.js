b252aba7d5873e13b3b71b8c12d21ecd6a08f3a8
momo990305@gmail.com <momo990305@gmail.com>
2023-07-26 23:50:15 +0900

    main slider control prev next > opacity

--- frontend/src/components/Main/Slider.js
+++ frontend/src/components/Main/Slider.js
@@ -9,8 +9,21 @@ export default function Slider() {
         setIndex(selectedIndex);
       };
 
+        const handleSliderMouseLeave = () => {
+          // 마우스 범위를 벗어날 때 자동으로 다시 투명해지게 합니다.
+          const controlNext = document.querySelector(".carousel-control-next");
+          controlNext && controlNext.classList.add("transparent");
+
+          const controlPrev = document.querySelector(".carousel-control-prev");
+          controlPrev && controlPrev.classList.add("transparent");
+        };
+
       return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel
+              activeIndex={index}
+              onSelect={handleSelect}
+              onMouseLeave={handleSliderMouseLeave} // mouseleave를 핸들하는 함수를 추가합니다.
+            >
           <Carousel.Item>
             <img className="slideImage" alt="slide_01" src="/imgs/Main/Slider/Slider1.png"/>
           </Carousel.Item>
