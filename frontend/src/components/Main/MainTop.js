9443ddcd3b15ce96fad2c2cf5f272fb2335cc830
momo990305@gmail.com <momo990305@gmail.com>
2023-10-03 15:28:38 +0900

    add MainTop Link

--- frontend/src/components/Main/MainTop.js
+++ frontend/src/components/Main/MainTop.js
@@ -4,13 +4,28 @@ import "./MainTop.css";
 export default function MainTop() {
     return (
         <div className="topDiv">
-            <img className="logo" src="/imgs/Main/MainImage2/1_logo.png"/>
+            <a className="logo_a" href="https://stoneinwell.com">
+                <img className="logo" src="/imgs/Main/MainImage2/1_logo.png"/>
+            </a>
+
             <img className="hr_image" src="/imgs/Main/MainImage2/2_hrLine.png"/>
+
             <div className="menu">
-                <img src="/imgs/Main/MainImage2/3_menu1.png"/>
-                <img src="/imgs/Main/MainImage2/3_menu2.png"/>
-                <img src="/imgs/Main/MainImage2/3_menu3.png"/>
-                <img src="/imgs/Main/MainImage2/3_menu4.png"/>
+                <a className="menu_a" href="https://stoneinwell.com/interviews">
+                    <img src="/imgs/Main/MainImage2/3_menu1.png"/>
+                </a>
+
+                <a className="menu_a" href="https://stoneinwell.com/interviews">
+                    <img className="interviews_button" src="/imgs/Main/MainImage2/3_menu2.png" alt="인터뷰" />
+                </a>
+
+                <a className="menu_a" href="https://stoneinwell.com/interviews">
+                    <img src="/imgs/Main/MainImage2/3_menu3.png"/>
+                </a>
+
+                <a className="menu_a" href="https://stoneinwell.com/interviews">
+                    <img src="/imgs/Main/MainImage2/3_menu4.png"/>
+                </a>
             </div>
         </div>
     );
--- frontend/src/pages/interviews/interviews.js
+++ frontend/src/pages/interviews/interviews.js
@@ -4,13 +4,14 @@ import { BrowserView, MobileView } from 'react-device-detect'
 import "./interviews.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//import { MainTop } from "./components/interviews";
+import { MainTop, ContactUs } from "../../components/Main";
 
 function interviews() {
 
     return (
         <div>
-            <h1>interviews</h1>
+            <MainTop/>
+            <ContactUs/>
         </div>
     );
 }
