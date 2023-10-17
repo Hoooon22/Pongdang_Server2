7eb0d8e5b1352fb69353ba29177ccab6f749190c
momo990305@gmail.com <momo990305@gmail.com>
2023-10-17 18:09:30 +0900

    Repository annotation

--- src/main/java/com/Pongdang_Server2/interview/repository/InterviewRepository.java
+++ src/main/java/com/Pongdang_Server2/interview/repository/InterviewRepository.java
@@ -1,7 +1,9 @@
 package com.Pongdang_Server2.interview.repository;
 
 import com.Pongdang_Server2.interview.entity.InterviewEntity;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InterviewRepository {
     void save(InterviewEntity interviewEntity);
 }
