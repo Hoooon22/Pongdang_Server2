311ee52c151099e332b5f5a097833ac16b5e73e1
Hoooon22 <momo990305@gmail.com>
2023-12-06 15:41:16 +0900

    Auto Search when page start

--- frontend/src/components/interviews/Search.js
+++ frontend/src/components/interviews/Search.js
@@ -28,6 +28,10 @@ export default function Search() {
     };
 
     useEffect(() => {
+        handleButtonClick();
+    }, []);
+
+    useEffect(() => {
         const img = imgRef.current;
         if (img) {
             const handleImageLoad = () => {
