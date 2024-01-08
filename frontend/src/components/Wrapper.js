61ab0a38357ff5ef74c0b86940b450611dcd6bcc
Hoooon22 <momo990305@gmail.com>
2024-01-09 08:15:51 +0900

    page switch animation

--- frontend/src/components/Wrapper.js
+++ frontend/src/components/Wrapper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { pageEffect } from './styles/animation';
+import { pageEffect } from '../styles/animation';
 
 const Wrapper = ({ children, ...rest }) => {
   return (
